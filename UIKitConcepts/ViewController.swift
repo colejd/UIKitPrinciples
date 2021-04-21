6b432cf4ef4ba42cdae317fca6feb501da085d45
Jonathan Cole <joncole04412@gmail.com>
2021-04-21 13:07:17 -0400

    Boilerplate project setup

---
+++ UIKitConcepts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitConcepts
+//
+//  Created by Jonathan Cole on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
